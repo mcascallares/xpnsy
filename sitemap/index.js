65bd31c1e3ee6f0304cf09505acb1558ee4b5bdd
Matias Cascallares <matiascas@gmail.com>
2013-04-30 10:41:45 +0200

    added about page skeleton

--- sitemap/index.js
+++ sitemap/index.js
@@ -1,12 +1,14 @@
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn
   , ensureLoggedOut = require('connect-ensure-login').ensureLoggedOut
 	, home = require('../routes/home')
+	, about = require('../routes/about')
 	, auth = require('../routes/auth')
   , dashboard = require('../routes/dashboard');
 
 
 exports.addRoutes = function(app, passport) {
 	app.get('/', ensureLoggedOut('/dashboard'), home.show);
+	app.get('/about', about.show);
 	app.get('/auth/facebook', passport.authenticate('facebook'));
 	app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/dashboard', failureRedirect: '/' }));
 	app.get('/logout', auth.logout);
