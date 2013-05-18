775dbad02d9499a05c12a13b0d5c03ad95a97b39
Matias Cascallares <matiascas@gmail.com>
2013-05-18 19:55:42 +0200

    mocks for charting

--- public/javascripts/xpnsy/widgets/chart-expense/widget.js
+++ public/javascripts/xpnsy/widgets/chart-expense/widget.js
@@ -7,18 +7,29 @@ define(['jquery',
 
       template: chartTemplate,
 
+      chartOptions: {
+        'title':'Expenses',
+        'width':400,
+        'height':300,
+        'is3D': true,
+        'chartArea': {'top':0, 'width': '100%', 'height': '100%'},
+        'legend': {'position': 'left'}
+      },
+
       initialize: function() {
         this.render();
       },
 
       render: function() {
-        // draw the widget skeleton
         this.$el.html(this.template());
-        this.showLastMonthPie();
+        this.showCurrentMonth();
         return this;
       },
 
-      showLastMonthPie: function() {
+      showCurrentMonth: function() {
+        $('.active', this.$el).removeClass('active');
+        $('.js-show-current-month', this.$el).addClass('active');
+
         var data = new google.visualization.DataTable();
         data.addColumn('string', 'Label');
         data.addColumn('number', 'Total amount');
@@ -29,15 +40,43 @@ define(['jquery',
           ['Transport', 75]
         ]);
 
-        var options = {
-          'title':'Expenses',
-          'width':400,
-          'height':400
-        };
-
-        this.drawChart(data, options);
+        this.drawChart(data, this.chartOptions);
       },
 
+
+      showCurrentYear: function() {
+        $('.active', this.$el).removeClass('active');
+        $('.js-show-current-year', this.$el).addClass('active');
+
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Label');
+        data.addColumn('number', 'Total amount');
+        data.addRows([
+          ['Travelling', 280],
+          ['Supermarket', 250],
+          ['Sport', 100],
+          ['Transport', 20]
+        ]);
+
+        this.drawChart(data, this.chartOptions);
+      },
+
+      showAll: function() {
+        $('.active', this.$el).removeClass('active');
+        $('.js-show-all', this.$el).addClass('active');
+
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Label');
+        data.addColumn('number', 'Total amount');
+        data.addRows([
+          ['Travelling', 100],
+          ['Supermarket', 200],
+          ['Sport', 50],
+          ['Transport', 50]
+        ]);
+
+        this.drawChart(data, this.chartOptions);
+      },
 
       drawChart: function(data, options) {
         var chart = new google.visualization.PieChart($('#chartContainer', this.$el)[0]);
@@ -45,7 +84,9 @@ define(['jquery',
       },
 
       events: {
-        //'click .js-submit': 'submit'
+        'click .js-show-current-month': 'showCurrentMonth',
+        'click .js-show-current-year': 'showCurrentYear',
+        'click .js-show-all': 'showAll'
       }
 
     });
