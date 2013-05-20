b82d5aa3b160369c314e58fe44e5a5db6e0591d3
Matias Cascallares <matiascas@gmail.com>
2013-05-20 15:53:46 +0200

    requirejs optimization config

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,23 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-requirejs');
 
   grunt.initConfig({
+
     pkg: grunt.file.readJSON('package.json'),
 
     requirejs: {
       compile: {
         options: {
-          mainConfigFile: 'public/javascripts/app.js',
           baseUrl: 'public/javascripts',
-          name: 'app',
-          include: ['app'],
-          out: 'public/javascripts/app.min.js'
+          dir: 'public/javascripts-built',
+          mainConfigFile: 'public/javascripts/main.js',
+          optimize: 'uglify', // set to 'none' if you want to disable
+          stubModules : ['text', 'hgn'],
+          modules: [
+              { name: 'main' },
+              { name: 'dashboard' }
+          ]
         }
       }
     }
-
   });
-
-
 };
