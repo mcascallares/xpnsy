ffdef6eb59be9bfbab23b6600295cd42259e3c28
Matias Cascallares <matiascas@gmail.com>
2013-05-20 18:56:20 +0200

    added trace log

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var express = require('express')
 
 // db connection initialization
 mongoose.connect(config.mongo.uri, {replSet: {socketOptions: {socketTimeoutMS: 200000}}});
+console.log(mongoose);
 
 // express setup
 var app = express();
